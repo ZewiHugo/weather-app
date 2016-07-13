e47723f4940dd7c63d94018bc7c293150765885b
aaaaaking <aaaaakinglevelup@gmail.com>
2016-07-12 23:31:59 -0400

    Weather app

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY='f9da005156d8f17c4a98cf7b7ce4b49b';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
